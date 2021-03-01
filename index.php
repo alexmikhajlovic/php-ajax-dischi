a8b6cdc7e21f1700e8b6328c8d4a997b00825792
Alex <alexmikhajlovic@gmail.com>
2021-03-01 15:28:32 +0100

    Milestone 1

--- index.php
+++ index.php
@@ -1,51 +1,61 @@
 <?php 
     $music = [
         [
+            'cover_path' => 'img/astroworld.jpg',
             'title' => 'Astroworld',
             'author' => 'Travis Scott',
             'year' => 2018,
         ],
         [
+            'cover_path' => 'img/rodeo.jpg',
             'title' => 'Rodeo',
             'author' => 'Travis Scott',
             'year' => 2015,
         ],
         [
+            'cover_path' => 'img/hollywoods-bleeding.jpg',
             'title' => 'Goodbyes',
             'author' => 'Post Malone',
             'year' => 2019,
         ],
         [
+            'cover_path' => 'img/toosie-slide.jpg',
             'title' => 'Toosie Slide',
             'author' => 'Drake',
             'year' => 2020,
         ],
         [
+            'cover_path' => 'img/my-way.png',
             'title' => 'My Way',
             'author' => 'Fetty Wap',
             'year' => 2015,
         ],
         [
+            'cover_path' => 'img/wavy.jpg',
             'title' => 'Wavy',
             'author' => 'Ty Dolla $ign',
             'year' => 2015,
         ],
         [
+            'cover_path' => 'img/black-hollywood.jpg',
             'title' => 'Promises',
             'author' => 'Wiz Khalifa',
             'year' => 2014,
         ],
         [
+            'cover_path' => 'img/summer-in-the-winter.jpg',
             'title' => 'Bank',
             'author' => 'Kid Ink',
             'year' => 2015,
         ],
         [
+            'cover_path' => 'img/freaky-friday.jpg',
             'title' => 'Freaky Friday',
             'author' => 'Chris Brown',
             'year' => 2019,
         ],
         [
+            'cover_path' => 'img/hollywoods-bleeding.jpg',
             'title' => 'Circles',
             'author' => 'Post Malone',
             'year' => 2019,
@@ -80,10 +90,25 @@
     <!-- MAIN -->
     <main>
 
-        <div class="music-box">
-            
-        </div>
+        <?php
+            foreach ($music as $music) { ?>
+                <div class="music-box">
 
+                    <!-- Cover -->
+                    <img src="<?php echo $music['cover_path'] ?>" alt="Cover">
+
+                    <!-- Title -->
+                    <h3><?php echo $music['title'] ?></h3>
+
+                    <!-- Author -->
+                    <p><?php echo $music['author'] ?></p>
+
+                    <!-- Year of release -->
+                    <p><small><?php echo $music['year'] ?></small></p>
+
+                </div>
+        <?php } ?>
+            
     </main>
     
 
